0db0646b2ef1459e493a3fa02832946d61767089
Vasek Smidl <Vasek.Smidl@gmail.com>
2026-03-27 23:13:05 +0100

    name

--- experiments/imitation/run_and_show.jl
+++ experiments/imitation/run_and_show.jl
@@ -49,27 +49,26 @@ function list_files(odir=prepath)
 end
 
 
-datasets = [ISPCDataset(s) for s in NeuroPlannerExperiments.IPC_PROBLEMS]
-archs = ["objectatom","objectatombipfe","atombinaryfe","atombinary","objectbinaryfe"]
-extractors = [Extractor(;architecture=a, graph_layers=l) for (a,l) in Iterators.product(archs, [1,2,4])]
+datasets = [ISPCDataset("ipc23_ferry")]
+archs = ["atombinaryfe"]
+extractors = [Extractor(;architecture=a, graph_layers=l) for (a,l) in Iterators.product(archs, [2])]
 
-message_blocks = [FFNN(hidden_dim = h, output_dim = h, layers = 1, layernorm = ln) for (h,ln) in Iterators.product([8,16,32], [true,false])]
-pooled_blocks = [FFNN(hidden_dim = h, output_dim = 1, layers = l, layernorm = ln) for (h,l,ln) in Iterators.product([8,16,32], 1:3, [true,false])]
+message_blocks = [FFNN(hidden_dim = h, output_dim = h, layers = 1, layernorm = ln) for (h,ln) in Iterators.product([32], [true])]
+pooled_blocks = [FFNN(hidden_dim = h, output_dim = 1, layers = l, layernorm = ln) for (h,l,ln) in Iterators.product([32], [2], [true])]
 
 models = [Model(message_pass_model = mp, pooled_model = o) for (mp,o) in Iterators.product(message_blocks, pooled_blocks)]
-losses = [Loss(s) for s in ["lstar"]]
+losses = [Loss(s) for s in ["lstar", "lgbfs"]]
 trains = [SupervisedTraining(;max_epoch = 10)]
 planners = [AStar(max_time = 30)]
-seeds = [1,2,3]
+seeds = [1, 2, 3]
 
 confs_dir = joinpath(prepath, "confs")
 mkpath(confs_dir)
 
-# Uncomment to (re-)generate configs:
-# confs = Iterators.product(datasets, models, extractors, losses, trains, planners, seeds)
-# for (i,c) in enumerate(confs)
-#     write_config(joinpath(confs_dir, "$(i).json"), c...)
-# end
+confs = Iterators.product(datasets, models, extractors, losses, trains, planners, seeds)
+for (i,c) in enumerate(confs)
+    write_config(joinpath(confs_dir, "$(i).json"), c...)
+end
 
 # Check progress
 files = map(readdir(confs_dir; join=true, sort=true)) do path
